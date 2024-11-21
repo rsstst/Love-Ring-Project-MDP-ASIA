440c7359ee71143075c8e1ebb3c49ef5d20ea1aa
rsstst <rsstst.ann@gmail.com>
2024-11-21 09:08:08 +0700

    basic dart file for login screen

---
+++ lib/screens/login.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(Object context) {
+    return Scaffold(
+        body: Column(
+      children: [Stack()],
+    ));
+  }
+}
