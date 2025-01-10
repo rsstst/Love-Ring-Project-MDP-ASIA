9938cac8a962fbc4c6d702f4fce70563f170562d
giayuuu <anggiayy831@gmail.com>
2025-01-10 21:00:45 +0700

    update user data +user +images

--- lib/data/user_data.dart
+++ lib/data/user_data.dart
@@ -1,7 +1,76 @@
 import 'package:pr_mobile_mdp/models/user.dart';
 
-var userList =[
-  User(Id: 'A1B2C3D', username: 'Gigih', password: 'passwordkuat'),
-  User(Id: '456DEFG', username: 'Nabil', password: 'katasandi'),
-  User(Id: 'ZYX1234', username: 'Sesil', password: 'sandiaman'),
+var userList = [
+  User(
+      Id: 'A1B2C3D',
+      username: 'Gigih',
+      password: 'passwordkuat',
+      nama: 'Gigih Satria',
+      loc: 'Jl. Kota Lama No.2',
+      profil: 'images/gigih.jpg',
+      desc:
+          'Aku adalah seorang pria barusia 21 tahun.Aku orang Malang, tetapi sekarang aku tinggal di Jakarta. Hobiku adalah berenang,sepak bolo, dan bernyanyi'),
+  User(
+      Id: '456DEFG',
+      username: 'Nabil',
+      password: 'katasandi',
+      nama: 'Muhammad Nabil Asyura',
+      loc: 'Jl. Ijen Nirwana  No.1',
+      profil: 'images/nabil.jpg',
+      desc:
+          'Aku adalah seorang pria barusia 18 tahun.Aku orang Sumatra, tetapi sekarang aku tinggal di Jakarta. Hobiku adalah berenang,sepak bolo, dan bernyanyi. Aku merupakan pemain Timnas'),
+  User(
+      Id: 'ZYX1234',
+      username: 'Yunanda',
+      password: 'sandiaman',
+      nama: 'Angga Aldi Yunanda',
+      loc: 'Jl. Ijen Nirwana  No.1',
+      profil: 'images/angga.jpg',
+      desc:
+          'Aku adalah seorang pria barusia 25 tahun.Aku orang Lombok, tetapi sekarang aku tinggal di Jakarta. Hobiku adalah berenang,sepak bolo, dan bernyanyi. Aku merupakan pemain aktor dan penyanyi'),
+  User(
+      Id: 'R1zK1N4Z4R',
+      username: 'Risky',
+      password: 'risky123',
+      nama: 'Rizky Nazar Mubarak Basloom',
+      loc: 'Jl. In Aja Dulu No. 1',
+      profil: 'images/risky.jpg',
+      desc:
+          'Aku adalah seorang pria berusia 28 tahun yang lahir di Bali dan kini tinggal di Jakarta. Hobiku adalah bermain sepak bola, bersepeda, dan mendalami seni peran. Sebagai seorang aktor, aku telah berperan dalam berbagai film dan sinetron yang menginspirasi banyak orang. Aku juga seorang penyanyi yang menikmati musik sebagai bagian dari hidupku.'),
+  User(
+      Id: 'R1zK1N4Z4R',
+      username: 'Risky',
+      password: 'risky123',
+      nama: 'Rizky Nazar Mubarak Basloom',
+      loc: 'Jl. In Aja Dulu No. 1',
+      profil: 'images/risky.jpg',
+      desc:
+          'Aku adalah seorang pria berusia 28 tahun yang lahir di Bali dan kini tinggal di Jakarta. Hobiku adalah bermain sepak bola, bersepeda, dan mendalami seni peran. Sebagai seorang aktor, aku telah berperan dalam berbagai film dan sinetron yang menginspirasi banyak orang. Aku juga seorang penyanyi yang menikmati musik sebagai bagian dari hidupku.'),
+  User(
+      Id: 'A1C7O2R8',
+      username: 'Fikri',
+      password: 'fikri123',
+      nama: 'Fikri Aditya Pratama',
+      loc: 'Jl. Bintang Utara No. 5',
+      profil: 'images/fikri.jpg',
+      desc:
+          'Aku adalah seorang pria berusia 30 tahun yang lahir di Bandung dan kini tinggal di Jakarta. Hobiku adalah bermain gitar, membaca buku, dan menjelajahi dunia seni peran. Sebagai seorang aktor, aku telah membintangi sejumlah film layar lebar, sinetron, dan teater. Aku juga aktif dalam berbagai kampanye sosial dan suka berbagi inspirasiku melalui platform digital.'),
+  User(
+      Id: 'B2M5R9F7',
+      username: 'Reza',
+      password: 'reza123',
+      nama: 'Reza Mahendra Putra',
+      loc: 'Jl. Raya No. 10, Jakarta',
+      profil: 'images/reza.jpeg',
+      desc:
+          'Aku seorang aktor berusia 29 tahun yang lahir di Surabaya. Aku sangat mencintai dunia seni, terutama dalam akting dan musik. Selain berperan dalam berbagai sinetron, aku juga sering terlibat dalam proyek film dan teater. Di luar dunia hiburan, aku juga memiliki hobi berolahraga, terutama bermain basket dan jogging, serta menghabiskan waktu bersama keluarga dan teman-teman.'),
+  User(
+      Id: 'M3X1B8O5T',
+      username: 'MaximeBouttier',
+      password: 'maxime123',
+      nama: 'Maxime Bouttier',
+      loc: 'Jl. Menteng No. 8, Jakarta',
+      profil: 'images/maxime.jpg',
+      desc:
+          'Aku seorang aktor dan penyanyi berusia 27 tahun yang lahir di Paris, Prancis, namun kini tinggal di Jakarta. Sejak kecil, aku sudah tertarik pada dunia seni, dan aku mulai berkarir di industri hiburan sejak remaja. Aku telah membintangi berbagai film dan sinetron yang membuatku dikenal luas. Selain itu, aku juga aktif dalam dunia musik, sering menulis lagu dan tampil di berbagai acara. Aku sangat menyukai olahraga dan petualangan, serta sering berbagi cerita dengan penggemarku di media sosial.')
 ];
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,7 +1,19 @@
 class User {
   final String Id;
   final String username;
+  final String nama;
+  final String loc;
   final String password;
+  final String profil;
+  final String desc;
 
-  User({required this.Id, required this.username, required this.password});
+  User({
+    required this.Id,
+    required this.username,
+    required this.password,
+    required this.profil,
+    required this.desc,
+    required this.loc,
+    required this.nama,
+  });
 }
